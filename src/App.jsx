9b3fbf38d394f19536fed0a742c7934c1b33b15e
avnashv <avinash.vijayan@wizr.ai>
2025-03-12 15:23:28 +0530

    Initial commit - Reusable Components

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import CustomButton from "./components/customButton/customButton";
+
+
+function App() {
+  return (
+    <div className="flex items-center justify-around gap-2 w-screen h-screen bg-gray-100">
+
+      <div className="flex flex-col gap-4 border-r-1 border-l-1 p-6 border-gray-300">
+        {/* -- Primary Buttons -- */}
+        <h4 className="text-lg text-gray-400">Primary</h4>
+        <CustomButton text="Button" onClick={() => alert('Hi Team!')} />
+        <CustomButton text="Button" disabled={true} showText={true} variant="primary" />
+        {/* <CustomButton text="Button" startIcon={true} endIcon={false} />
+        <CustomButton text="Button" startIcon={false} endIcon={true} />
+        <CustomButton text="Button" startIcon={false} endIcon={false} />
+        <CustomButton showText={false} rounded="full" /> */}
+      </div>
+
+      <div className="flex flex-col gap-4 border-r-1 border-l-1 p-6 border-gray-300">
+        <h4 className="text-lg text-gray-400">Secondary</h4>
+        {/* -- Secondary Buttons -- */}
+        <CustomButton text="Button" variant="secondary" />
+        <CustomButton text="Button" disabled={true} variant="secondary" />
+      </div>
+
+      <div className="flex flex-col gap-4 border-r-1 border-l-1 p-6 border-gray-300">
+        <h4 className="text-lg text-gray-400">Teritary</h4>
+        {/* -- Teritary Buttons -- */}
+        <CustomButton text="Button" variant="teritary" />
+        <CustomButton text="Button" disabled={true} variant="teritary" />
+      </div>
+
+      <div className="flex flex-col gap-4 border-r-1 border-l-1 p-6 border-gray-300">
+        <h4 className="text-lg text-gray-400">Icon</h4>
+        {/* -- Icon Button -- */}
+        <CustomButton variant="icon" disabled={false} showText={false} startIcon={false} endIcon={true} />
+        <CustomButton variant="icon" disabled={true} showText={false} startIcon={false} endIcon={true} />
+      </div>
+
+      <div className="flex flex-col gap-4 border-r-1 border-l-1 p-6 border-gray-300">
+        <h4 className="text-lg text-gray-400">Chips</h4>
+        {/* -- Chips -- */}
+        <CustomButton text="Chips" variant="chips" rounded="full" startIcon={true} endIcon={true} />
+        <CustomButton text="Chips" disabled={true} variant="chips" rounded="full" />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/customButton/customButton.jsx
@@ -0,0 +1,196 @@
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import clsx from "clsx";
+import AddIcon from "../../assets/icons/add.svg";
+import GrayAdd from "../../assets/icons/gray-add.svg";
+import BlueAdd from "../../assets/icons/blue-add.svg";
+import BlueArrowUp from "../../assets/icons/blue-arrow-up.svg";
+import WhiteArrowUp from "../../assets/icons/white-arrow-up.svg";
+
+const buttonStyles = {
+    primary: "hover:bg-sky-600 active:bg-sky-700 disabled:bg-sky-300",
+    secondary: "hover:border-red-500 active:border-red-500 focus:border-red-500 disabled:border-red-300",
+    teritary: "hover:bg-slate-600 active:bg-slate-900 disabled:bg-slate-300",
+    icon: "hover:border-[#009CDC] hover:text-white bg-green-500",
+    chips: "border border-[#CBDBE4] bg-transparent hover:border-[#009CDC] hover:bg-white",
+};
+
+// Define custom colors for each button variant
+const buttonColors = {
+    primary: "#009CDC",
+    secondary: "transparent", // Gray-600 equivalent for secondary
+    teritary: "#1E293B", // Slate-900 equivalent for tertiary
+    icon: "transparent",
+    chips: "transparent",
+};
+
+// Get the appropriate icon based on the button variant
+const getIcon = (variant, isHovered, isClicked, isDisabled) => {
+    if (variant === "chips") {
+        return isDisabled ? GrayAdd : isHovered || isClicked ? BlueAdd : GrayAdd;
+    }
+
+    switch (variant) {
+        case "primary":
+            return AddIcon;
+        case "secondary":
+            return BlueArrowUp;
+        case "teritary":
+            return WhiteArrowUp;
+        case "icon":
+            return BlueArrowUp;
+        default:
+            return AddIcon;
+    }
+};
+
+// Custom Button Component
+const CustomButton = ({
+    text = "Button",
+    variant = "primary",
+    disabled = false,
+    startIcon = true,
+    endIcon = true,
+    showText = true,
+    rounded = "lg",
+    iconSize = "40px",
+    onClick,
+}) => {
+    const isIconButton = variant === "icon";
+    const isChipButton = variant === "chips";
+    const isSecondaryButton = variant === "secondary";
+
+    const buttonSize = isIconButton ? `w-[${iconSize}] h-[${iconSize}]` : isChipButton ? "w-[114px] h-[32px]" : "";
+    const padding = isChipButton ? "px-4 py-2" : isIconButton ? "p-0" : "px-4 py-2";
+
+    const [isHovered, setIsHovered] = useState(false);
+    const [isClicked, setIsClicked] = useState(false);
+
+    return (
+        <Button
+            onClick={() => {
+                setIsClicked(true);
+                if (onClick) onClick();
+            }}
+            disabled={disabled}
+            variant="contained"
+            disableRipple
+
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            onFocus={() => setIsHovered(true)}
+            onBlur={() => setIsClicked(false)}
+
+            sx={{
+                backgroundColor: isIconButton || isChipButton || isSecondaryButton
+                    ? "transparent"
+                    : variant === "primary"
+                        ? "#009CDC"
+                        : variant === "teritary"
+                            ? "#253746"
+                            : "transparent",
+
+                color: isChipButton ? (isHovered || isClicked ? "#009CDC" : "#818B94")
+                    : isIconButton ? "#009CDC"
+                        : variant === "secondary" ? "#009CDC" : "#fff",
+                fontWeight: "bold",
+                borderRadius: rounded === "full" ? "9999px" : "12px",
+                border: isIconButton
+                    ? "1px solid #FFFFFF"
+                    : isChipButton
+                        ? "1px solid #CBDBE4"
+                        : variant === "secondary"
+                            ? "0px solid #009CDC"
+                            : "none",
+                width: isIconButton ? iconSize : isChipButton ? "114px" : "auto",
+                height: isIconButton ? iconSize : isChipButton ? "32px" : "auto",
+                minWidth: isIconButton ? iconSize : "auto",
+                minHeight: isIconButton ? iconSize : "auto",
+                padding: isIconButton ? "0px" : isChipButton ? "var(--sds-size-space-300) var(--sds-size-space-400)" : "12px 16px",
+                gap: "8px",
+                boxShadow: "none",
+                "&:hover": {
+                    backgroundColor: isChipButton ? "#FFFFFF"
+                        : isIconButton ? "transparent"
+                            : variant === "primary" ? "#006D9A"
+                                : variant === "secondary" ? "transparent" : "#1A2731",
+
+                    border: isChipButton ? "1px solid #009CDC"
+                        : isIconButton ? "1px solid #009CDC"
+                            : variant === "secondary" ? "1px solid #009CDC" : "",
+
+                    color: isChipButton || isIconButton ? "#009CDC"
+                        : variant === "secondary" ? "#009CDC" : "",
+                },
+
+                "&:active": {
+                    backgroundColor: isChipButton ? "#FFFFFF"
+                        : isIconButton ? "transparent"
+                            : variant === "primary" ? "#009CDC"
+                                : variant === "teritary" ? "#253746" : "transparent",
+
+                    border: isIconButton ? "1px solid #009CDC"
+                        : variant === "secondary" ? "1px solid #009CDC" : "",
+
+                    color: isChipButton ? "#009CDC"
+                        : isIconButton ? "#FFFFFF"
+                            : variant === "secondary" ? "#009CDC" : "",
+                },
+
+                "&:focus": {
+                    border: isChipButton ? "1px solid #009CDC"
+                        : isIconButton ? "1px solid #009CDC"
+                            : variant === "secondary" ? "1px solid #009CDC" : "",
+                    backgroundColor: variant === "secondary" ? "transparent" : "",
+                    color: isChipButton ? "#009CDC"
+                        : isIconButton ? "#FFFFFF" : variant === "secondary" ? "#009CDC" : "",
+                },
+
+                "&.Mui-disabled": {
+                    border: isChipButton ? "1px solid #CBDBE4" : variant === "secondary" ? "1px solid #009CDC" : "",
+                    backgroundColor: isChipButton || isIconButton
+                        ? "transparent"
+                        : variant === "primary"
+                            ? "#009CDC"
+                            : variant === "secondary"
+                                ? "transparent"
+                                : "#253746",
+
+                    color: isChipButton ? "#818B94" : variant === "secondary" ? "#009CDC" : "white",
+                    opacity: 0.5,
+                },
+            }}
+
+            className={clsx(
+                "flex items-center justify-center gap-2 transition-all duration-200",
+                buttonStyles[variant] || buttonStyles["primary"],
+                rounded === "full" ? "rounded-full" : "rounded-lg",
+                buttonSize,
+                padding
+            )}
+        >
+            {/* Start Icon */}
+            {startIcon && (
+                <img
+                    src={getIcon(variant, isHovered, isClicked, disabled)} // Default/disabled state
+                    alt="Start Icon"
+                    className="w-4 h-4"
+                />
+            )}
+
+            {/* Button Text (optional) */}
+            {showText && <span>{text}</span>}
+
+            {/* End Icon */}
+            {endIcon && (
+                <img
+                    src={getIcon(variant, isHovered, isClicked, disabled)} // Default/disabled state
+                    alt="End Icon"
+                    className="w-4 h-4"
+                />
+            )}
+        </Button>
+    );
+};
+
+export default CustomButton;
