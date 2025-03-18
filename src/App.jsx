adb6098c54e8335be192040ca18a89b9c7fae4fb
sreejithaWizr <sreejitha.mc@wizr.ai>
2025-03-18 16:13:39 +0530

    button style restored

--- src/App.jsx
+++ src/App.jsx
@@ -24,43 +24,48 @@ function App() {
       <div className="flex gap-8 border-b-1 border-t-1 pt-2 rounded-4xl pb-2 px-2 shadow-xl border-gray-300 text-center bg-white">
 
         <div className="flex items-center gap-4 justify-center border-r-1 border-l-1 p-6 border-gray-300 rounded-4xl shadow-md">
-          <h1 className="text-xl font-bold text-gray-500">Buttons</h1>
+          <h1 className="text-xl font-bold text-gray-500">Custom Buttons</h1>
         </div>
 
-        {/* Primary Buttons */}
-        <div className="flex flex-col gap-4 border-r border-l p-6 border-gray-300 rounded-4xl shadow-md">
+        <div className="flex flex-col gap-4 border-r-1 border-l-1 p-6 border-gray-300 rounded-4xl shadow-md">
+          {/* -- Primary Buttons -- */}
           <h4 className="text-lg text-gray-400">Primary</h4>
-          <CustomButton text="Button" onClick={() => alert("Hi Team!")} />
-          <CustomButton text="Button" disabled variant="primary" />
+          <CustomButton text="Button" onClick={() => alert('Hi Team!')} />
+          <CustomButton text="Button" disabled={true} showText={true} variant="primary" />
+          {/* <CustomButton text="Button" startIcon={true} endIcon={false} />
+        <CustomButton text="Button" startIcon={false} endIcon={true} />
+        <CustomButton text="Button" startIcon={false} endIcon={false} />
+        <CustomButton showText={false} rounded="full" /> */}
         </div>
 
-        {/* Secondary Buttons */}
-        <div className="flex flex-col gap-4 border-r border-l p-6 border-gray-300 rounded-4xl shadow-md">
+        <div className="flex flex-col gap-4 border-r-1 border-l-1 p-6 border-gray-300 rounded-4xl shadow-md">
           <h4 className="text-lg text-gray-400">Secondary</h4>
+          {/* -- Secondary Buttons -- */}
           <CustomButton text="Button" variant="secondary" />
-          <CustomButton text="Button" disabled variant="secondary" />
+          <CustomButton text="Button" disabled={true} variant="secondary" />
         </div>
 
-        {/* Tertiary Buttons */}
-        <div className="flex flex-col gap-4 border-r border-l p-6 border-gray-300 rounded-4xl shadow-md">
-          <h4 className="text-lg text-gray-400">Tertiary</h4>
-          <CustomButton text="Button" variant="tertiary" />
-          <CustomButton text="Button" disabled variant="tertiary" />
+        <div className="flex flex-col gap-4 border-r-1 border-l-1 p-6 border-gray-300 rounded-4xl shadow-md">
+          <h4 className="text-lg text-gray-400">Teritary</h4>
+          {/* -- Teritary Buttons -- */}
+          <CustomButton text="Button" variant="teritary" />
+          <CustomButton text="Button" disabled={true} variant="teritary" />
         </div>
 
-        {/* Icon Buttons */}
-        <div className="flex flex-col gap-4 border-r border-l p-6 border-gray-300 rounded-4xl shadow-md">
+        <div className="flex flex-col gap-4 border-r-1 border-l-1 p-6 border-gray-300 rounded-4xl shadow-md">
           <h4 className="text-lg text-gray-400">Icon</h4>
-          <CustomButton variant="icon" showText={false} endIcon />
-          <CustomButton variant="icon" disabled showText={false} endIcon />
+          {/* -- Icon Button -- */}
+          <CustomButton variant="icon" disabled={false} showText={false} startIcon={false} endIcon={true} />
+          <CustomButton variant="icon" disabled={true} showText={false} startIcon={false} endIcon={true} />
         </div>
 
-        {/* Chips */}
-        <div className="flex flex-col gap-4 border-r border-l p-6 border-gray-300 rounded-4xl shadow-md">
+        <div className="flex flex-col gap-4 border-r-1 border-l-1 p-6 border-gray-300 rounded-4xl shadow-md">
           <h4 className="text-lg text-gray-400">Chips</h4>
-          <CustomButton text="Chips" variant="chips" rounded="full" startIcon endIcon />
-          <CustomButton text="Chips" disabled variant="chips" rounded="full" />
+          {/* -- Chips -- */}
+          <CustomButton text="Chips" variant="chips" rounded="full" startIcon={true} endIcon={true} />
+          <CustomButton text="Chips" disabled={true} variant="chips" rounded="full" />
         </div>
+
       </div>
 
       {/* -- Custom Dropdown Component -- */}
