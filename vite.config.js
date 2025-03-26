e0a0d02848f0ca2cd35c737d036fe680bdfe1c39
avnashv <avinash.vijayan@wizr.ai>
2025-03-26 11:45:50 +0530

    Added changes for creating library.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),],
+    tailwindcss(),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.js"),
+      name: "ReactMuiTailwindUI",
+      fileName: (format) => `react-mui-tailwind-ui.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "@mui/material"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "@mui/material": "MaterialUI",
+        },
+      },
+    },
+  },
 })
