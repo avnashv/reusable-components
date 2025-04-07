c465dc88ad3d0ae7a0357898d36a3ec6be467468
avnashv <avinash.vijayan@wizr.ai>
2025-04-07 15:14:13 +0530

    added OffcanvasModal to export in index.js.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 export { default as CustomButton } from "./components/customButton/customButton";
 export { default as CustomCheckboxField } from "./components/customCheckboxField/customCheckboxField";
 export { default as CustomDatePicker } from "./components/customDatePicker/customDatePicker";
-// export { default as CustomDropDown } from "./components/customDropDown/customDropDown";
 export { default as CustomDropDown } from "./components/customDropDown/customDropDown";
 export { default as CustomInputField } from "./components/customInputField/customInputField";
 export { default as CustomPagination } from "./components/customPagination/customPagination";
@@ -12,3 +11,4 @@ export { default as CustomTable } from "./components/customTable/customTable";
 export { default as CustomToggle } from "./components/customToggle/customToggle";
 export { default as CustomDateRangePicker } from "./components/customDateRangePicker/customDateRangePicker";
 export { default as customTimePicker } from "./components/customTimePicker/customTimePicker";
+export { default as OffcanvasModal } from "./components/customOffcanvas/OffcanvasModal";
