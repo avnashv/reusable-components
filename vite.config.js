b119f5146d1d2f10b89fe332dc036c861e79277e
sreejithaWizr <sreejitha.mc@wizr.ai>
2025-04-09 18:13:44 +0530

    filter inprogress

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   build: {
-    sourcemap: true,
     lib: {
-      entry: path.resolve(__dirname, 'src/index.js'),
-      name: 'ReactMuiTailwindUI',
+      entry: path.resolve(__dirname, "src/index.js"),
+      name: "ReactMuiTailwindUI",
       fileName: (format) => `react-mui-tailwind-ui.${format}.js`,
-      formats: ['es', 'umd'],
     },
     rollupOptions: {
-      external: [
-        'react',
-        'react-dom',
-        '@mui/material',
-        '@emotion/react',
-        '@emotion/styled',
-        '@mui/icons-material'
-      ],
+      external: ["react", "react-dom", "@mui/material"],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          '@mui/material': 'MaterialUI',
-          '@emotion/react': 'EmotionReact',
-          '@emotion/styled': 'EmotionStyled',
-          '@mui/icons-material': 'MuiIcons'
+          react: "React",
+          "react-dom": "ReactDOM",
+          "@mui/material": "MaterialUI",
         },
       },
     },
